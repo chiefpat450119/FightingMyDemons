f9ce7b70e496367ee1a57b9acac4b6fcd75f94ab
anushathukral <107315079+anushathukral@users.noreply.github.com>
2023-11-19 15:26:03 -0800

    Update cardgame.py

--- cardgame.py
+++ cardgame.py
@@ -12,19 +12,18 @@ FPS = 60
 WHITE = (255, 255, 255)
 
 # Load face-down images
-card_images_face_down = [pygame.image.load("assets/CardBackside.png") for _ in range(16)]
+card_images_face_down = [pygame.image.load("assets/CardBackside.png") for i in range(12)]
 
 # Game variables
-cards = [i // 2 for i in range(16)] 
+cards = [i // 2 for i in range(12)] 
 random.shuffle(cards)
 flipped_cards = []
-selected_card = None
 game_over = False
 background = pygame.image.load("assets/background.jpg")
 background = pygame.transform.scale(background, (WIDTH, HEIGHT))
 
 # Load a single set of face-up images for all cards
-card_images_face_up = [pygame.image.load(f"assets/{name}") for name in ["ArrowClusterCard.png", "AxeCard.png", "IceShrinkCard.png", "SpearCard.png"]]
+card_images_face_up = [pygame.image.load(f"assets/{name}") for name in ["ArrowClusterCard.png", "AxeCard.png", "IceShrinkCard.png", "SpearCard.png", "SwordCard.png", "SwordClusterCard.png"]]
 
 # Pygame setup
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -61,16 +60,22 @@ def draw_board():
 
 
 def check_match():
-    if len(flipped_cards) == 2:
-        if cards[flipped_cards[0]] == cards[flipped_cards[1]]:
-            # Cards match
-            flipped_cards.clear()
-            if all(i in flipped_cards for i in range(16)):
-                global game_over
-                game_over = True
-        else:
-            # Cards do not match, flip them back after a delay
-            pygame.time.set_timer(pygame.USEREVENT, 1000)
+  if len(flipped_cards) == 2:
+      if cards[flipped_cards[0]] == cards[flipped_cards[1]]:
+          matched_indices = list(set(flipped_cards))
+          print("Matched Indices:", matched_indices)
+          for index in matched_indices:
+              cards[index] = -1
+
+          if all(card == -1 for card in cards):
+              global game_over
+              game_over = True
+              print("Game Over!")
+      else:
+          print("Cards do not match")
+          pygame.time.set_timer(pygame.USEREVENT, 500)
+
+
 
 
 def main():
@@ -93,10 +98,7 @@ def main():
                         check_match()
 
             elif event.type == pygame.USEREVENT:
-              if len(flipped_cards) > 0:
-                flipped_cards.pop()
-                if len(flipped_cards) > 0:
-                    flipped_cards.pop()
+              flipped_cards.clear()
 
         screen.blit(background, (0, 0))
         draw_board()
